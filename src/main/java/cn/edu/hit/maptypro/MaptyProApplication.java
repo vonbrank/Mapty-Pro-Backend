fc9bddded97592a10b939be21390d4d640c845f4
vonbrank <vonbrank@outlook.com>
2022-11-16 13:03:19 +0800

    chore: 首次提交，初始化项目配置

---
+++ src/main/java/cn/edu/hit/maptypro/MaptyProApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hit.maptypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaptyProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/hit/maptypro/MaptyProApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.edu.hit.maptypro;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaptyProApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
