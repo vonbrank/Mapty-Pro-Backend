c631cc728af65186d0417270936f535591db5634
vonbrank <vonbrank@outlook.com>
2022-11-20 10:10:37 +0800

    feat (User Controller): 用户注册、登录重构

--- src/main/java/cn/edu/hit/maptypro/controller/UserController.java
+++ src/main/java/cn/edu/hit/maptypro/controller/UserController.java
@@ -8,7 +8,6 @@ import cn.edu.hit.maptypro.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class UserController {
@@ -20,19 +19,14 @@ public class UserController {
     @PostMapping(value = "api/login")
     @ResponseBody
     public Response login(@RequestBody User requestUser) {
-        String username = requestUser.getUsername();
-        if (username != null) username = HtmlUtils.htmlEscape(username);
-        String email = requestUser.getEmail();
-        if (email != null) email = HtmlUtils.htmlEscape(email);
 
-        User user = null;
-        User userByUsername = null;
-        User userByEmail = null;
-        if (username != null && !username.isEmpty())
-            userByUsername = userService.getByName(username, requestUser.getPassword());
-        if (email != null && !email.isEmpty()) userByEmail = userService.getByEmail(email, requestUser.getPassword());
-        user = userByUsername;
-        if (user == null) user = userByEmail;
+        requestUser = userService.userHtmlEscape(requestUser);
+
+        if (!userService.checkLoginUserValidation(requestUser)) {
+            return ResponseFactory.buildResult(ResponseCode.FAIL, "Input required.", null);
+        }
+
+        User user = userService.getByNameAndEmail(requestUser);
 
         if (user == null) {
             return ResponseFactory.buildResult(ResponseCode.FAIL, "User does not exist.", null);
@@ -45,27 +39,20 @@ public class UserController {
     @PostMapping(value = "api/register")
     @ResponseBody
     public Response register(@RequestBody User requestUser) {
-        String username = requestUser.getUsername();
-        if (username != null) username = HtmlUtils.htmlEscape(username);
-        String email = requestUser.getEmail();
-        if (email != null) email = HtmlUtils.htmlEscape(email);
 
-        User user = null;
-        User userByUsername = null;
-        User userByEmail = null;
-        if (username != null && !username.isEmpty()) userByUsername = userService.getByName(username);
-        if (email != null && !email.isEmpty()) userByEmail = userService.getByEmail(email);
-        if (userByUsername != null || userByEmail != null) {
+        User user = userService.userHtmlEscape(requestUser);
+
+        if (!userService.checkRegisterUserValidation(user)) {
+            return ResponseFactory.buildResult(ResponseCode.FAIL, "Input required.", null);
+        }
+
+        if (userService.checkUserRepetition(user)) {
             return ResponseFactory.buildResult(ResponseCode.FAIL, "User has existed.", null);
         }
 
-        String password = requestUser.getPassword();
-        if (username != null && email != null && password != null) {
-            User newUser = new User(username, email, password);
-            userService.add(newUser);
-            return ResponseFactory.buildResult(ResponseCode.SUCCESS, "Register successfully.", newUser);
-        } else {
-            return ResponseFactory.buildResult(ResponseCode.FAIL, "Input required.", null);
-        }
+        User newUser = new User(user.getUsername(), user.getEmail(), user.getPassword());
+        userService.add(newUser);
+        return ResponseFactory.buildResult(ResponseCode.SUCCESS, "Register successfully.", newUser);
+
     }
 }
--- src/main/java/cn/edu/hit/maptypro/service/UserService.java
+++ src/main/java/cn/edu/hit/maptypro/service/UserService.java
@@ -4,6 +4,7 @@ import cn.edu.hit.maptypro.dao.UserDAO;
 import cn.edu.hit.maptypro.entity.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 @Service
 public class UserService {
@@ -29,6 +30,62 @@ public class UserService {
 
     public User getByEmail(String email, String password) {
         return userDAO.getByEmailAndPassword(email, password);
+    }
+
+    public User getByNameAndEmail(User user) {
+        user = userHtmlEscape(user);
+
+        User userByUsername = null;
+        User userByEmail = null;
+        if (user.getUsername() != null && !user.getUsername().isEmpty())
+            userByUsername = getByName(user.getUsername(), user.getPassword());
+        if (user.getEmail() != null && !user.getEmail().isEmpty())
+            userByEmail = getByEmail(user.getEmail(), user.getPassword());
+
+        if (userByUsername != null) return userByUsername;
+        return userByEmail;
+    }
+
+    public boolean checkUserRepetition(User user) {
+        user = userHtmlEscape(user);
+
+        User userByUsername = null;
+        User userByEmail = null;
+        if (user.getUsername() != null && !user.getUsername().isEmpty()) userByUsername = getByName(user.getUsername());
+        if (user.getEmail() != null && !user.getEmail().isEmpty()) userByEmail = getByEmail(user.getEmail());
+
+        return userByUsername != null || userByEmail != null;
+    }
+
+    public User userHtmlEscape(User user) {
+        if (user == null) return null;
+        String username = user.getUsername();
+        if (username == null) username = "";
+        else username = HtmlUtils.htmlEscape(username);
+        String email = user.getEmail();
+        if (email == null) email = "";
+        else email = HtmlUtils.htmlEscape(email);
+
+        User newUser = new User();
+        newUser.setId(user.getId());
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(user.getPassword());
+
+        return newUser;
+    }
+
+    public boolean checkRegisterUserValidation(User user) {
+        if (user == null) return false;
+        return user.getUsername() != null && !user.getUsername().isEmpty() &&
+                user.getPassword() != null && !user.getPassword().isEmpty();
+    }
+
+    public boolean checkLoginUserValidation(User user) {
+        if (user == null) return false;
+        return ((user.getUsername() != null && !user.getUsername().isEmpty()) ||
+                (user.getEmail() != null && !user.getEmail().isEmpty())) &&
+                (user.getPassword() != null && !user.getPassword().isEmpty());
     }
 
     public void add(User user) {
